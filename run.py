1efde38686f89640f95976ec6db8d143533439d6
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-04-03 20:51:33 +0000

    change run.py

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
-CREDS = credentials.from_service_account_file('creds.json')
+CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scope(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Love_Sandwitch_spreadsheet')
